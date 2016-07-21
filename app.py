7edf166a484aad630c9bced3a9cbd68e27390108
Emanuel de Souza <emanuelcds@gmail.com>
2016-07-20 22:31:22 -0700

    feat(*): Implemented web interface and binary packager.

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+from bottle import run
+from bottle import request, response
+from bottle import get, post
+
+from shamrock.runtime import SlotRuntime
+from shamrock.backend import ShamrockSweepsBackend
+
+
+
+BASE_DIR = os.path.dirname(sys.argv[0])
+SETTINGS_FILE = os.path.join(BASE_DIR, "settings.json")
+
+Runtime = SlotRuntime(SETTINGS_FILE, ShamrockSweepsBackend)
+
+@post('/slot/play')
+def slot_play():
+    try:
+        body = request.json()
+    except:
+        response.status = 400
+        return "Bad Request"
+
+    if "bet" not in body.keys():
+        response.status = 400
+        return "Bad Request - Missing 'bet' attribute."
+    if "game" not in body.keys():
+        response.status = 400
+        return "Bad Request - Missing 'game' attribute."
+
+    result = Runtime.handle(str(body.get("game")), float(body.get("bet")))
+    response.status = 201
+    response.headers['Content-Type'] = 'application/json'
+    return result
+
+@get('/credits')
+def get_credits():
+    credits = Runtime.backend.get_credits()
+    result = {
+        "credits": credits
+    }
+    request.status = 200
+    response.headers['Content-Type'] = 'application/json'
+    return result
+
+
+run(host='127.0.0.1', port=int(os.getenv('PORT', 5000)))
--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from distutils.core import setup
-import py2exe
 
-setup(console=['main.py'])
+import py2exe, os, _mssql, pymssql
+import decimal, uuid
+
+data_files = []
+data_files.append(os.path.join(os.path.split(pymssql.__file__)[0], 'ntwdblib.dll'))
+py2exe_options = {"py2exe":{"includes": ['decimal', 'uuid', '_mssql'],
+                "dll_excludes":["mswsock.dll",
+                "powrprof.dll",
+                "user32.dll",
+                "shell32.dll",
+                "wsock32.dll",
+                "advapi32.dll",
+                "kernel32.dll",
+                "ntwdblib.dll",
+                "ws2_32.dll",
+                "oleaut32.dll",
+                "ole32.dll",
+                            ],
+}}
+
+setup(console=["app.py"], options=py2exe_options)
+
+
