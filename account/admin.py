3777ecf62671020b700aece81d97d4a0de9aba4b
Emanuel de Souza <emanuelcds@gmail.com>
2017-01-04 00:36:08 -0200

    feat(*): removing is_superuser field for user that aren't superusers.

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from account.models import Pin, Location
 
 # Register your models here.
 
 admin.site.register(Pin)
 admin.site.register(Location)
+
+
+class MyUserAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = []
+        if not request.user.is_superuser:
+            self.exclude.append('Permissions')
+            self.exclude.append('is_superuser')
+        return super(MyUserAdmin, self).get_form(request, obj, **kwargs)
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
