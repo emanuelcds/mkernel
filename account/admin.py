2da558bc50f6ff415630d725bf2dab2e823a5b88
Emanuel de Souza <emanuelcds@gmail.com>
2017-01-04 00:41:51 -0200

    fix typo

--- account/admin.py
+++ account/admin.py
@@ -12,7 +12,7 @@ class MyUserAdmin(admin.ModelAdmin):
     def get_form(self, request, obj=None, **kwargs):
         self.exclude = []
         if not request.user.is_superuser:
-            self.exclude.append('permissions')
+            self.exclude.append('user_permissions')
             self.exclude.append('groups')
             self.exclude.append('is_superuser')
         return super(MyUserAdmin, self).get_form(request, obj, **kwargs)
